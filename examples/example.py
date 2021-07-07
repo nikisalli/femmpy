f94127dcaf4849236bd3645ad6b9bd66f9b60e09
nikisalli <niki.salli@gmail.com>
2021-07-07 02:35:17 +0200

    better example comments

--- examples/example.py
+++ examples/example.py
@@ -1,15 +1,22 @@
 import femmpy
 
-fem = femmpy.femm_file()
+fem = femmpy.femm_file()  # initialize file generator
+# add iron square with vertices on the specified coordinates
 fem.polygon([[1, 0], [0, 0], [0, 1], [1, 1]], 'iron')
+# add circuit named lol, real part of current is 2Amps, imaginary part is 0Amps, circuit type: 1 for series 0 for parallel
 fem.circuit("lol", 2, 0, 1)
+# add left part of the coil made of 18AWG copper wire, circuit id is 1, the coil has 100 entering windings
 fem.polygon([[0, 0], [-1, 0], [-1, 1], [0, 1]], '18AWG', 1, 100)
+# add right part of the coil made of 18AWG copper wire, circuit id is 1, the coil has 100 exiting windings
 fem.polygon([[2, 0], [1, 0], [1, 1], [2, 1]], '18AWG', 1, -100)
 
 # GENERATE FEM FILE
 try:
+    # generate .fem file
     fem.generate("./example.fem")
+    # mesh the geometry and solve
     fem.solve()
+    # save image with plotted magnetic field
     fem.save_plot("./example.jpg")
 except RuntimeError as e:
     print(e)
